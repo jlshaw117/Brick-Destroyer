fdac64862d171794caca5da6e250091fa97951aa
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-13 11:01:21 -0800

    Remove rect

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ document.addEventListener('DOMContentLoaded', () => {
     let canvas = document.getElementById('screen');
     let screen = canvas.getContext('2d');
 
-    screen.fillRect(10, 10, 50, 50);
+    
 });
