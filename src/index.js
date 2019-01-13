5e1fb1b31694a3a36ddf3356459f2c11644834d0
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-13 10:41:33 -0800

    Set up webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let canvas = document.getElementById('screen');
+    let screen = canvas.getContext('2d');
+
+    screen.fillRect(10, 10, 50, 50);
+});
