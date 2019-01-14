606ac103d8f19b684bd0d01a42f4bef72231d955
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-14 09:40:08 -0800

    Adjust edge and line width

--- src/modules/brick.js
+++ src/modules/brick.js
@@ -4,11 +4,11 @@ class Brick {
     constructor(x,y) {
         this.x = x;
         this.y = y;
-        this.edge = 20;
+        this.edge = 19;
     }
 
     draw(screen, color) {
-        screen.lineWidth = 3;
+        screen.lineWidth = 1;
         screen.fillStyle = color;
         screen.rect(this.x,this.y,this.edge,this.edge);
         screen.stroke();
