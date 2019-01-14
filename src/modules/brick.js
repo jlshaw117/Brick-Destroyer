a6d2262db0aad82c78cc4e85c7054c6161f502df
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-14 09:59:37 -0800

    Add value to constructor and begin path and close path

--- src/modules/brick.js
+++ src/modules/brick.js
@@ -1,18 +1,21 @@
 
 class Brick {
 
-    constructor(x,y) {
+    constructor(x,y, value) {
         this.x = x;
         this.y = y;
         this.edge = 19;
+        this.value = value;
     }
 
     draw(screen, color) {
+        screen.beginPath();
         screen.lineWidth = 1;
         screen.fillStyle = color;
         screen.rect(this.x,this.y,this.edge,this.edge);
         screen.stroke();
         screen.fill();
+        screen.closePath();
     }
 }
 
