fb23e1f725cbd03e02129faa8d8dacad53ba45d9
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-14 11:46:13 -0800

    Adjust size of brick

--- src/modules/brick.js
+++ src/modules/brick.js
@@ -4,13 +4,13 @@ class Brick {
     constructor(x,y, value) {
         this.x = x;
         this.y = y;
-        this.edge = 19;
+        this.edge = 35;
         this.value = value;
     }
 
     draw(screen, color) {
         screen.beginPath();
-        screen.lineWidth = 1;
+        // screen.lineWidth = 1;
         screen.fillStyle = color;
         screen.rect(this.x,this.y,this.edge,this.edge);
         screen.stroke();
