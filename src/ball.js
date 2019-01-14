742ef2d11f7cff074019cc47012d78e8a006d20f
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-13 19:40:45 -0800

    Add constructor to ball class

--- src/ball.js
+++ src/ball.js
@@ -1,6 +1,17 @@
 class Ball {
 
-    constructor(game, startX, StartY) {
+    constructor(startX, startY) {
+        this.x = startX;
+        this.y = startY;
+        this.dx = 0;
+        this.dy = 0;
+        this.speed = 2;
+        this.radius = 5;
+    }
+
+    move(newX, newY) {
 
     }
 }
+
+export { Ball };
