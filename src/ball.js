940de4e84694503ce9800ad36f279bc1abb8a433
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-13 20:20:53 -0800

    Refactor ball draw

--- src/ball.js
+++ src/ball.js
@@ -1,12 +1,17 @@
 class Ball {
 
-    constructor(startX, startY) {
+    constructor(radius, startX, startY) {
         this.x = startX;
         this.y = startY;
         this.dx = 0;
         this.dy = 0;
         this.speed = 2;
-        this.radius = 5;
+        this.radius = radius;
+    }
+
+    draw(screen) {
+        screen.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        screen.fill();
     }
 
     move(newX, newY) {
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let canvas = document.getElementById('screen');
     let screen = canvas.getContext('2d');
     let paddle = new Paddle(canvas, 10, 100);
+    let ball = new Ball(5, canvas.width / 2, paddle.y - 5);
     let gameStart = false;
 
     canvas.addEventListener('mousemove', (e) => {
@@ -15,9 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
         if (gameStart === false) {
             ball.x = mousePos.x;
         }
-        
-        // mousePos = calculateMousePos(e);
     });
+
+    const calculateMousePos = (e) => {
+        let rect = canvas.getBoundingClientRect();
+        let root = document.documentElement;
+        let mouseX = e.clientX - rect.left - root.scrollLeft;
+        let mouseY = e.clientY - rect.top - root.scrollTop;
+        return {
+            x: mouseX,
+            y: mouseY
+        };
+    };
+
 
     canvas.addEventListener('click', () => {
         if (!gameStart) {
@@ -28,8 +39,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function draw() {
         canvas.width = canvas.width;
-        screen.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-        screen.fill();
+        ball.draw(screen);
+        // screen.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+        // screen.fill();
         screen.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
 
         if (gameStart) {
@@ -79,17 +91,7 @@ document.addEventListener('DOMContentLoaded', () => {
        return (distFromEdgeL / (paddle.width / 2) - 1) * 2;
     };
 
-    const calculateMousePos = (e) => {
-        let rect = canvas.getBoundingClientRect();
-        let root = document.documentElement;
-        let mouseX = e.clientX - rect.left - root.scrollLeft;
-        let mouseY = e.clientY - rect.top - root.scrollTop;
-        return {
-            x: mouseX,
-            y: mouseY
-        };
-    };
-
+    
     draw();
     // let interval = setInterval(draw, 1000/frames);
 });
