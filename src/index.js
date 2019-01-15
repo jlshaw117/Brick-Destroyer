2d5e172ece0eb7d315f0f977090af6ccf8b0684c
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-14 16:18:15 -0800

    Refactor game logic into game class

--- src/index.js
+++ src/index.js
@@ -2,77 +2,11 @@ import Ball from './modules/ball';
 import Paddle from './modules/paddle';
 import Brick from './modules/brick';
 import * as level from './modules/levels';
+import Game from './modules/game';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    let canvas = document.getElementById('screen');
-    let screen = canvas.getContext('2d');
-    let paddle = new Paddle(canvas, 10, 100);
-    let ball = new Ball(8, canvas.width / 2, paddle.y - 8);
-    let gameStart = false;
-    let bricks = [];
-    for (let y = 0; y < level.one.length; y++) {
-        for (let x = 0; x < level.one[0].length; x++) {
-            if (level.one[y][x] > 0) {
-                let b = new Brick(x * 40, y * 40, level.one[y][x]);
-                bricks.push(b);
-            }
-        }
-    }
-
-
-    canvas.addEventListener('mousemove', (e) => {
-        let mousePos = calculateMousePos(e);
-        paddle.x = mousePos.x - (paddle.width / 2);
-        if (gameStart === false) {
-            ball.x = mousePos.x;
-        }
-    });
-
-    const calculateMousePos = (e) => {
-        let rect = canvas.getBoundingClientRect();
-        let root = document.documentElement;
-        let mouseX = e.clientX - rect.left - root.scrollLeft;
-        let mouseY = e.clientY - rect.top - root.scrollTop;
-        return {
-            x: mouseX,
-            y: mouseY
-        };
-    };
-
-
-    canvas.addEventListener('click', () => {
-        if (!gameStart) {
-            gameStart = true;
-            ball.dy = -1;
-        }
-    });
-
-    function draw() {
-        canvas.width = canvas.width;
-        paddle.draw(screen);
-        bricks.forEach((brick) => brick.draw(screen, 'red'));
-        ball.draw(screen);
-        
-
-        if (gameStart) {
-
-            ball.collisionWithWall(canvas);
-            ball.collisionWithTop();
-            ball.collisionWithPaddle(paddle);
-            ball.collisionWithGround(canvas);
-            bricks.forEach((brick) => ball.collisionWithBrick(brick));
-            bricks.forEach((brick, i) => {
-                if (brick.value <= 0) bricks.splice(i, 1);
-            });
-    
-            ball.x += ball.dx * ball.speed;
-            ball.y += ball.dy * ball.speed;
-        }
-
-        requestAnimationFrame(draw);
-    }
-    
-
-    draw();
+    let game = new Game();
+    console.log(game.bricks);
+    game.play();
 });
--- src/modules/game.js
+++ src/modules/game.js
@@ -9,9 +9,9 @@ class Game {
         this.lives = lives;
         this.roundStart = false;
         this.canvas = document.getElementById('screen');
-        this.secreen = this.canvas.getContext('2d');
+        this.screen = this.canvas.getContext('2d');
         this.paddle = new Paddle(this.canvas, 10, 100);
-        this.ball = new Ball(8, this.canvas.width / 2, paddle.y - 8);
+        this.ball = new Ball(8, this.canvas.width / 2, this.paddle.y - 8);
         this.bricks = [];
         this.level = 1;
         this.levels = levels;
@@ -21,21 +21,70 @@ class Game {
     buildLevel() {
         for (let y = 0; y < this.currentLevel.length; y++) {
             for (let x = 0; x < this.currentLevel[0].length; x++) {
-                let brick = new Brick(x * 40, y * 40, this.currentLevel[y][x]);
-                this.bricks.push(brick);
+                if (this.currentLevel[y][x] > 0) {
+                    let brick = new Brick(x * 40, y * 40, this.currentLevel[y][x]);
+                    this.bricks.push(brick);
+                }
             }
-            
         }
     }
 
     play() {
+        console.log(this.bricks);
         this.buildLevel();
-
+        let game = this;
         this.canvas.addEventListener('click', () => {
             if (!this.roundStart) {
                 this.roundStart = true;
                 this.ball.dy =-1;
             }
         });
+
+        this.canvas.addEventListener('mousemove', (e) => {
+            let mousePos = calculateMousePos(e);
+            this.paddle.x = mousePos.x - (this.paddle.width / 2);
+            if (this.roundStart === false) {
+                this.ball.x = mousePos.x;
+            }
+        });
+
+        const calculateMousePos = (e) => {
+            let rect = this.canvas.getBoundingClientRect();
+            let root = document.documentElement;
+            let mouseX = e.clientX - rect.left - root.scrollLeft;
+            let mouseY = e.clientY - rect.top - root.scrollTop;
+            return {
+                x: mouseX,
+                y: mouseY
+            };
+        };
+
+        function draw() {
+            game.canvas.width = game.canvas.width;
+            game.paddle.draw(game.screen);
+            game.bricks.forEach((brick) => brick.draw(game.screen));
+            game.ball.draw(game.screen);
+
+            if (game.roundStart) {
+
+                game.ball.collisionWithWall(game.canvas);
+                game.ball.collisionWithTop();
+                game.ball.collisionWithPaddle(game.paddle);
+                game.ball.collisionWithGround(game.canvas);
+                game.bricks.forEach((brick) => game.ball.collisionWithBrick(brick));
+                game.bricks.forEach((brick, i) => {
+                    if (brick.value <= 0) game.bricks.splice(i, 1);
+                });
+
+                game.ball.x += game.ball.dx * game.ball.speed;
+                game.ball.y += game.ball.dy * game.ball.speed;
+            }
+
+            requestAnimationFrame(draw);
+        }
+
+        draw();
     }
 }
+
+export default Game;
