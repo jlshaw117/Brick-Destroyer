2b29b48347f0202fd22a5bc0c550e7886c4387d0
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-15 09:59:58 -0800

    Add a 0 key in position hash

--- src/modules/brick.js
+++ src/modules/brick.js
@@ -10,7 +10,8 @@ class Brick {
 
     draw(screen) {
 
-        const COLORS = {
+        const POSITIONS = {
+            0: {x: 0, y: 120},
             1: {x: 0, y: 120},
             2: { x: 360, y: 80},
             3: {x: 320, y: 80}
@@ -19,7 +20,7 @@ class Brick {
         screen.beginPath();
 
         const img = document.getElementById('sprites');
-        screen.drawImage(img, COLORS[this.value].x, COLORS[this.value].y, 30, 30, this.x, this.y, this.edge, this.edge);
+        screen.drawImage(img, POSITIONS[this.value].x, POSITIONS[this.value].y, 30, 30, this.x, this.y, this.edge, this.edge);
         
 
     }
