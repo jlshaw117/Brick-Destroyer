20d5ed88397aa94e11b3d715faf2f5129cf47a23
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-14 17:54:59 -0800

    Add image for paddle

--- src/modules/paddle.js
+++ src/modules/paddle.js
@@ -3,14 +3,17 @@
 class Paddle {
 
     constructor(canvas, height, width) {
-        this.height = height;   
+        this.height = 30;   
         this.width = width;
         this.x = canvas.width / 2 - (width / 2);
-        this.y = canvas.height - height - 10;
+        this.y = canvas.height - 30 - 10;
     }
 
     draw(screen) {
-        screen.fillRect(this.x, this.y, this.width, this.height);
+        // screen.fillRect(this.x, this.y, this.width, this.height);
+
+        const img = document.getElementById('sprites');
+        screen.drawImage(img, 0, 200, 100, 30, this.x, this.y, this.width, this.height);
     }
 }
 
