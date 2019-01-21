9223bcc85da3305ab68b756586194eb396fe567d
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-21 08:05:10 -0800

    Reset paddle on next level

--- src/modules/game.js
+++ src/modules/game.js
@@ -42,6 +42,7 @@ class Game {
         this.roundStart = false;
         this.paddle.catch = true;
         this.paddle.blaster = false;
+        this.paddle.width = 100;
         this.level += 1;
         this.currentLevel = this.levels[(this.level - 1) % this.levels.length];
         this.buildLevel();
