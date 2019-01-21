af3353dead68bd864f5caf1e107d6d20fcf52a09
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-20 18:53:23 -0800

    Add bullet class

---
+++ src/modules/bullet.js
@@ -0,0 +1,22 @@
+
+class Bullet {
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = 10;
+        this.height = 15;
+        this.speed = -1;
+    }
+
+    draw(screen) {
+        screen,fillStyle = 'blue';
+        screen.fillRect(x, y, width, height);
+        screen.arc(x + (width / 2), y, width / 2 - 0.5, 0, Math.PI, true);
+        screen.arc(x + (width / 2), y + height, width / 2 - 0.5, 0, Math.PI, false);
+        screen.stroke();
+        screen.fill();
+    } 
+}
+
+export default Bullet;
